4e027600c1d286ad51546aaac4dd8030682b4eae
Yassine Rassy <yassinDevox>
2023-03-03 21:28:20 +0100

    done with variables

---
+++ essentials1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	
+	//variables 
+	//RAM (many cellule with 8bits ): size memory
+	//char , int , long, float, double
+	//work with variables ( declaration & intialization)
+	char c='c';
+	int i=2;
+	float f=2.2;
+	//io:input/output
+	printf("\nc=%c\ni=%d\nf=%f",c,i,f);
+	 
+	
+	//how to provide choices ( if/else, switch)
+	return 0;
+}
+
